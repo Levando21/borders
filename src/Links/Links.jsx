88239200ea3987dfb62976d66ebe5431f3ff9467
Lev <leozolotukhin@gmail.com>
2024-08-22 20:44:37 +0200

    push

---
+++ src/Links/Links.jsx
@@ -0,0 +1,25 @@
+/** @format */
+import "./Links.module.css";
+export default function Links() {
+	return (
+		<div>
+			<ul>
+				<li>
+					<a href="">
+						<span className="name-platform">YouTube</span>
+						<span className="eicon-youtube"></span>
+					</a>
+				</li>
+				<li>
+					<a href=""></a>
+				</li>
+				<li>
+					<a href=""></a>
+				</li>
+				<li>
+					<a href=""></a>
+				</li>
+			</ul>
+		</div>
+	);
+}
